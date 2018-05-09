aeac81c5d9ada1e2672875c6325b0e43b957bfc4
seablue001 <1465768310@qq.com>
2018-05-09 14:11:03 +0800

    配置

---
+++ src/assets/js/index.js
@@ -0,0 +1,8 @@
+//createjs相关库
+import easeljs from 'imports-loader?this=>window!easeljs';
+import preloadjs from 'imports-loader?this=>window!preloadjs';
+import soundjs from 'imports-loader?this=>window!soundjs';
+import tweenjs from 'imports-loader?this=>window!tweenjs';
+
+
+console.log(easeljs);
